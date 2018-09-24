835cb8b90e29347583988af7dafd4bbc409c6775
Bert Peters <bert@bertptrs.nl>
2018-09-24 17:19:26 +0200

    Implement magic number checking.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+extern crate fuse;
+
 mod backend;
+mod wfs;
 use backend::FileBackend;
 use std::env;
+use std::ffi::OsStr;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let mut backend = FileBackend::from_name(&args[1])?;
+    let backend = FileBackend::from_name(&args[1])?;
+    let fs = wfs::FileSystem::from_backend(backend);
+    let options = ["-o", "ro", "-o", "fsname=hello"]
+        .iter()
+        .map(|o| o.as_ref())
+        .collect::<Vec<&OsStr>>();
+    fuse::mount(fs, &args[2], &options)?;
     Ok(())
 }
---
+++ src/wfs.rs
@@ -0,0 +1,78 @@
+extern crate fuse;
+extern crate libc;
+extern crate slice_cast;
+
+use backend::Backend;
+use self::libc::c_int;
+
+#[derive(Default)]
+pub struct FileSystem<T: Backend> {
+    backend: T,
+}
+
+const MAGIC_CONST: [u32; 4] = [
+    0x00c0ffeeu32,
+    0x00000000,
+    0xf00d1350,
+    0x0000beef,
+];
+
+impl <T: Backend> FileSystem<T> {
+
+    fn magic_check(&mut self) -> Result<(), c_int> {
+        let mut rbuf = [0u8; 16];
+        if let Err(_) = self.backend.read(0, &mut rbuf) {
+            return Err(libc::EINVAL)
+        }
+
+        let magic_read: &[u32] = unsafe { slice_cast::cast(&rbuf) };
+
+        if magic_read == MAGIC_CONST {
+            Ok(())
+        } else {
+            // Panic because this method cannot return error states.
+            panic!("Invalid magic number");
+        }
+    }
+
+    pub fn from_backend(backend: T) -> FileSystem<T> {
+        FileSystem{
+            backend: backend
+        }
+    }
+
+}
+
+impl <T: Backend> fuse::Filesystem for FileSystem<T> {
+
+    fn init(&mut self, _req: &fuse::Request) -> Result<(), c_int> {
+        self.magic_check()
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use backend::RAMBackend;
+
+    #[test]
+    fn test_magic_number() {
+        let mut backend: RAMBackend = Default::default();
+        let magic_string: &[u8] = unsafe { slice_cast::cast(&MAGIC_CONST) };
+        let _ = backend.write(0, &magic_string);
+        let mut instance = FileSystem::from_backend(backend);
+        match instance.magic_check() {
+            Ok(()) => {},
+            Err(_) => assert!(false)
+        };
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_magic_number_failure() {
+        let backend: RAMBackend = Default::default();
+        let mut instance = FileSystem::from_backend(backend);
+        let _ = instance.magic_check();
+    }
+}
